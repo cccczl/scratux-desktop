8cb827cf3bb09a170d07e98c297645062e80f386
Joan Ciprià <joancipria@gmail.com>
2020-02-23 20:14:20 +0100

    Fix translation files path

--- src/translations/i18n.js
+++ src/translations/i18n.js
@@ -7,18 +7,18 @@ let app = electron.app ? electron.app : electron.remote.app
 module.exports = i18n;
 
 function i18n() {
-    if(fs.existsSync(path.join(__dirname, app.getLocale() + '.json'))) {
-         loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, app.getLocale() + '.json'), 'utf8'))
-    }
-    else {
-         loadedLanguage = JSON.parse(fs.readFileSync(path.join(__dirname, 'en.json'), 'utf8'))
-    }
+     if (fs.existsSync(path.join(__dirname, app.getLocale() + '.json'))) {
+          loadedLanguage = JSON.parse(fs.readFileSync(path.join(__static, 'translations', app.getLocale() + '.json'), 'utf8'))
+     }
+     else {
+          loadedLanguage = JSON.parse(fs.readFileSync(path.join(__static, 'translations', 'en.json'), 'utf8'))
+     }
 }
 
-i18n.prototype.__ = function(phrase) {
-    let translation = loadedLanguage[phrase]
-    if(translation === undefined) {
-         translation = phrase
-    }
-    return translation
+i18n.prototype.__ = function (phrase) {
+     let translation = loadedLanguage[phrase]
+     if (translation === undefined) {
+          translation = phrase
+     }
+     return translation
 }
